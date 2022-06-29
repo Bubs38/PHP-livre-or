38e6f43ff2d6a9bcb3f774b7cffba77804c0e80f
Bubs38 <burgain.aurelien@gmail.com>
2022-06-29 23:45:54 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,72 @@
+<?php
+require_once 'class/Message.php';
+require_once 'class/GuestBook.php';
+
+$errors = null;
+$success = false;
+
+if (isset($_POST['username'], $_POST['message'])) {
+  $message = new Message($_POST['username'], $_POST['message']);
+  if ($message->isValid()) {
+
+    $guestbook = new GuestBook(__DIR__ . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'messages');
+    $guestbook->addMessage($message);
+    $success = true;
+    $_POST = [];
+
+  } else {
+    
+    $errors = $message->getErrors();
+
+  }
+}
+
+
+$title = "Livre d'or";
+require 'elements/header.php';
+
+?>
+
+<div class="container">
+  <h1>Livre d'or</h1>
+
+  <?php if (!empty($errors)) : ?>
+    <div class="alert alert-danger">
+      Formulaire invalide
+    </div>
+  <?php endif ?>
+
+  <?php if ($success) : ?>
+    <div class="alert alert-success">
+      Message Envoyé
+    </div>
+  <?php endif ?>
+
+  <form action="" method="post">
+
+    <div class="form-group">
+      <input value="<?= htmlentities($_POST['username'] ?? '')?>" type="text" name="username" placeholder="Votre pseudo" class="form-control <?= isset($errors['username']) ?  'is-invalid' : ''?>">
+
+      <?php if (isset($errors['username'])): ?>
+        <div class="invalid-feedback"><?= $errors['username']?></div>
+        <?php endif ?>
+      </div>
+      
+      <div class="form-group">
+        <textarea name="message" placeholder="Votre message" class="form-control <?= isset($errors['message']) ? 'is-invalid' : ''?>"><?= htmlentities($_POST['message'] ?? '')?></textarea>
+
+        <?php if (isset($errors['message'])): ?>
+          <div class="invalid-feedback"><?= $errors['message']?></div>
+        <?php endif ?>
+    </div>
+
+    <button type="submit" class="btn btn-primary">Envoyer</button>
+  </form>
+</div>
+
+
+
+<?php
+// var_dump($_Post['message']);
+require 'elements/footer.php';
+?>
